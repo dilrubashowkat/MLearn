ac9bf1e7297b6f5b4708733cf9b6ff22582e33cc
Dilruba Showkat <dilrubashowkat@gmail.com>
2017-12-21 05:53:51 -0600

    iris dataset imported

--- testpy1.py
+++ testpy1.py
@@ -1,7 +1,15 @@
 from sklearn import tree;
+from sklearn.datasets import load_iris;
+
 feature = [[140,0],[130,0],[150,1],[170,1]];
 label = ["apple","apple","orange","orange"];
 clf = tree.DecisionTreeClassifier();
 clf = clf.fit(feature,label);
 print(clf.predict([[160,1]]));
 print("hello world");
+
+##Irish data set exploration##
+iris_flower = load_iris();
+print(iris_flower.feature_names);
+print(iris_flower.target_names);
+print("end world");
