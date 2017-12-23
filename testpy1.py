e5a06bc23e13b348c5d7c36d2e265ed1a5d2b882
Dilruba Showkat <dilrubashowkat@gmail.com>
2017-12-23 01:49:08 -0600

    numpy added

--- testpy1.py
+++ testpy1.py
@@ -1,3 +1,4 @@
+import numpy as np;
 from sklearn import tree;
 from sklearn.datasets import load_iris;
 
@@ -13,7 +14,23 @@ iris_flower = load_iris();
 print(iris_flower.feature_names);
 print(iris_flower.target_names);
 print(iris_flower.data[0]);
-print("end world war");
+print("end world");
 
 for i in range(len(iris_flower.target)):
     print("Data ID %d: label %s, features %s" , i,iris_flower.target[i],iris_flower.data[i]);
+
+test_idx=[0,50,100];
+
+#training
+train_target = np.delete(iris_flower.target,test_idx);
+train_data = np.delete(iris_flower.data,test_idx,axis=0);
+
+#testing
+test_target = iris_flower.target[test_idx];
+test_data = iris_flower.data[test_idx];
+
+clf = tree.DecisionTreeClassifier();
+clf = clf.fit(train_data,train_target);
+
+print(test_target);
+print(clf.predict(test_data));
