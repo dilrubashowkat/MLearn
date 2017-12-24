1b561cf126d454de27fa43204ccc9b768985cc43
Dilruba Showkat <dilrubashowkat@gmail.com>
2017-12-24 04:21:10 -0600

    conda install -c pydot command failing

--- testpy1.py
+++ testpy1.py
@@ -10,27 +10,39 @@ print(clf.predict([[160,1]]));
 print("hello world");
 
 ##Irish data set exploration##
-iris_flower = load_iris();
-print(iris_flower.feature_names);
-print(iris_flower.target_names);
-print(iris_flower.data[0]);
+iris = load_iris();
+print(iris.feature_names);
+print(iris.target_names);
+print(iris.data[0]);
 print("end world");
 
-for i in range(len(iris_flower.target)):
-    print("Data ID %d: label %s, features %s" , i,iris_flower.target[i],iris_flower.data[i]);
+for i in range(len(iris.target)):
+    print("Data ID %d: label %s, features %s" , i,iris.target[i],iris.data[i]);
 
 test_idx=[0,50,100];
 
 #training
-train_target = np.delete(iris_flower.target,test_idx);
-train_data = np.delete(iris_flower.data,test_idx,axis=0);
+train_target = np.delete(iris.target,test_idx);
+train_data = np.delete(iris.data,test_idx,axis=0);
 
 #testing
-test_target = iris_flower.target[test_idx];
-test_data = iris_flower.data[test_idx];
+test_target = iris.target[test_idx];
+test_data = iris.data[test_idx];
 
 clf = tree.DecisionTreeClassifier();
 clf = clf.fit(train_data,train_target);
 
 print(test_target);
 print(clf.predict(test_data));
+
+import graphviz;
+dot_data = tree.export_graphviz(clf, out_file=None);
+graph = graphviz.Source(dot_data);
+graph.render("iris");
+dot_data = tree.export_graphviz(clf, out_file=None,
+                         feature_names=iris.feature_names,
+                         class_names=iris.target_names,
+                         filled=True, rounded=True,
+                         special_characters=True);
+graph = graphviz.Source(dot_data);
+graph.write_pdf("iris.pdf");
